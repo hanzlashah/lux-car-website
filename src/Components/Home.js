5788369a6d3d6ac776c2ec82449a0d004d290b9a
hanzlashah <108062644+hanzlashah@users.noreply.github.com>
2024-10-29 03:02:24 +0500

    nav and hero section is add

---
+++ src/Components/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import TopNavigation from "./TopNavigation";
+import Hero from "./Hero";
+
+function Home() {
+  return (
+    <div>
+      <TopNavigation />
+      <Hero />
+    </div>
+  );
+}
+
+export default Home;
